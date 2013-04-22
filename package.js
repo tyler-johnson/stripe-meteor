4f156b5d8208fd449a5689cc259a95e673ef1252
Tyler Johnson <tyler@vintyge.com>
2013-04-22 13:09:25 -0600

    added stripe checkout

--- package.js
+++ package.js
@@ -6,5 +6,6 @@ Npm.depends({ "stripe": "1.3.0" });
 
 Package.on_use(function (api) {
 	api.add_files('stripe_client.js', 'client');
+	api.add_files('stripe_checkout.js', 'client');
 	api.add_files('stripe_server.js', 'server');
 });
