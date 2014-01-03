da39f3dae477fa9ee12c802f96be3bba270409a8
Tyler Johnson <tyler@vintyge.com>
2014-01-03 09:48:19 -0700

    updated node-stripe and stripe-checkout

--- package.js
+++ package.js
@@ -2,14 +2,12 @@ Package.describe({
 	summary: "Stripe.js and Node-Stripe brought to Meteor."
 });
 
-Npm.depends({ "stripe": "2.1.0-rc3" });
+Npm.depends({ "stripe": "2.3.2" });
 
 Package.on_use(function (api) {
 	
-	 // ensure backwards compatibility with Meteor pre-0.6.5
-	if (api.export){
-	    api.export('STRIPEMETEOR');
-	}
+	// ensure backwards compatibility with Meteor pre-0.6.5
+	if (api.export) api.export('STRIPEMETEOR');
     
 	api.add_files('stripe_client.js', 'client');
 	api.add_files('stripe_checkout.js', 'client');
