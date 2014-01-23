3dee84ed6bb286b4ec6f40139ec24ad6f36522d6
Mikael Lirbank <mikael@lirbank.com>
2014-01-23 14:12:36 -0800

    Updated stripe-node npm dependency

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	summary: "Stripe.js and Node-Stripe brought to Meteor."
 });
 
-Npm.depends({ "stripe": "2.3.2" });
+Npm.depends({ "stripe": "2.3.4" });
 
 Package.on_use(function (api) {
 	
